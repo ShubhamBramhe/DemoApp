54d5ca3bbaa9914990e44f2bf02d07e3c9639670
ShubhamBramhe <161293238+ShubhamBramhe@users.noreply.github.com>
2025-01-07 16:00:17 +0530

    Add new File Demo2 (#6)

---
+++ DemoApp/Models/Demo2.cs
@@ -0,0 +1,12 @@
+﻿namespace DemoApp.Models
+{
+    public class Demo2
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+
+    }
+}
